5ddb7d787e7312721a31a868c97c72ddb6a64e27
ASUS <ASUS@LAPTOP-1V72JD3E>
2024-08-21 21:28:40 +0800

    添加项目文件。

---
+++ test.c
@@ -0,0 +1,99 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//int main()
+//{
+//	int sum = 1;
+//	while (sum <= 10)
+//	{
+//		printf("打代码:%d\n",sum);
+//		sum++;
+//	}
+//	if (sum = 10)
+//		printf("好offfer\n");
+//	return 0;
+//
+//}
+//
+//
+//
+
+//int Add(int x, int y)
+//{
+//	int z = x + y;
+//	return z;
+//}
+//int main()
+//{
+//	int num1 = 0;
+//	int num2 = 0;
+//	scanf(" %d %d", &num1 ,&num2);
+//	//int  sum = num1 + num2;
+//	int sum = Add(num1, num2);
+//	printf("sum=%d", sum);
+//
+//
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int arr[10] = { 100,200,3000,400,500,600,700,88,99 };
+//	int i = 0;
+//	while (i < 10)
+//	{
+//		printf("%d\n", arr[i]);
+//		i++;
+//	}
+//
+//	return 0;
+//}
+
+//
+//int main()
+//{
+//	char ch[4] = { '他','恶','份' };
+//	int i = 0;
+//	while (i < 4)
+//	{
+//		printf("%c\n", ch[i]);
+//			i++;
+//
+//	}
+//	return 0;
+//}
+
+
+
+//
+
+
+//int main()
+//{
+//	int a =(int) 3.14;
+//	printf("%d", a);
+//
+//
+//
+//	return 0;
+//}
+
+
+
+
+//int main()
+//{
+//	int a = 1;
+//	int b = 2;
+//	/*int max = 0;
+//	if (a>b)
+//		max = a;
+//	else max = b;*/
+//	int max = a > b ? a : b;
+//	printf("%d", max);
+//	
+//	return 0;
+//}
+
+//typedef unsigned int  u_int;
